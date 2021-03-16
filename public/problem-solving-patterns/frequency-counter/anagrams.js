9bac38c5bd4ac554dddc3f0b3902d6d422d20771
¨kemken077¨ <kemken077@gmail.com>
2021-03-16 10:29:36 -0300

    Removed comment

--- public/problem-solving-patterns/frequency-counter/anagrams.js
+++ public/problem-solving-patterns/frequency-counter/anagrams.js
@@ -5,7 +5,6 @@
  * @returns {boolean}
  */
 function validAnagram(str1, str2){
-    // add whatever parameters you deem necessary - good luck!
     if (str1.length !== str2.length) {
         return false;
     }
