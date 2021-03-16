11eaa0db67f9bc85634ca7279b26c103df9363f3
¨kemken077¨ <kemken077@gmail.com>
2021-03-16 09:06:10 -0300

    Re arrange file structure, avoid fav icon request error

---
+++ problem-solving-patterns/scripts/frequency-counter/frequency-counter.js
@@ -0,0 +1,6 @@
+var FREQUENCY_COUNTER_NAMESPACE = {};
+
+FREQUENCY_COUNTER_NAMESPACE.pattern_name = 'Frequency counter';
+
+
+console.log(`${FREQUENCY_COUNTER_NAMESPACE.pattern_name} script works!`);
